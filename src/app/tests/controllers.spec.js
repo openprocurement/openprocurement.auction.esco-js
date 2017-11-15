1ae209f7c611b99b4aec2a58feb128665ffdd64f
Scandie <grizly.vl@gmail.com>
2017-11-15 12:47:03 +0200

    Update controller tests due to changes in AuctionCtl for esco
    - Fix filter tests

--- src/app/tests/controllers.spec.js
+++ src/app/tests/controllers.spec.js
@@ -2,11 +2,12 @@ describe('auctionTest', function () {
 
   beforeEach(module('auction'));
 
-  var controller, scope, AuctionUtils;
+  let controller, scope, AuctionUtils, rootScope;
 
   beforeEach(inject(function(_$controller_, _$rootScope_, AuctionUtils){
     scope = _$rootScope_.$new();
-    controller = _$controller_('AuctionController', {$scope: scope, AuctionUtils: AuctionUtils});
+    rootScope = _$rootScope_;
+    controller = _$controller_('AuctionController', {$scope: scope, AuctionUtils: AuctionUtils, $rootScope: rootScope});
   }));
 
 //$scope.post_bid
@@ -14,23 +15,30 @@ describe('auctionTest', function () {
     expect(scope.post_bid).toBeDefined();
   });
 
-  it('should push alerts', function () {
+  it('should push alerts', angular.mock.inject(['AuctionUtils', function(AuctionUtils) {
     let length = scope.alerts.length;
-    scope.form.bid = -1;
+    rootScope.get_annual_costs_reduction = function(bidder_id) {};
+    rootScope.auction_doc = {noticePublicationDate: null, NBUdiscountRate:null};
+    rootScope.minimal_bid = {amount: "12"};
+    AuctionUtils.npv = function() {return "12"};
+    scope.form.BidsForm = {$valid: true};
     expect(!!scope.post_bid()).toEqual(false);
     expect(scope.alerts.length).toEqual(length + 1);
-  });
+  }]));
 
-  it('should warn if the proposal you have submitted coincides with a proposal of the other participant bid', function () {
+  it('should warn if the proposal you have submitted coincides with a proposal of the other participant bid',
+      angular.mock.inject(['AuctionUtils', function(AuctionUtils) {
     let length = scope.alerts.length;
-    scope.form.BidsForm = {};
-    scope.form.BidsForm.$valid = true;
-    scope.minimal_bid = {amount:2};
-    scope.post_bid(2);
+    scope.form.BidsForm = {$valid: true};
+    rootScope.auction_doc = {noticePublicationDate: null, NBUdiscountRate:null};
+    rootScope.get_annual_costs_reduction = function(bidder_id) {};
+    rootScope.minimal_bid = {amount: "12"};
+    AuctionUtils.npv = function() {return "12"};
+    scope.post_bid(14, 20, 85);
 
     expect(scope.alerts.length).toEqual(length + 1);
     expect(scope.alerts[0].msg && scope.alerts[0].type=='warning').toEqual(true);
-  });
+  }]));
 //$scope.edit_bid
   it('should be Defined', function () {
     expect(scope.edit_bid).toBeDefined();
@@ -47,19 +55,18 @@ describe('auctionTest', function () {
     expect(scope.max_bid_amount()).toBe(0);
   });
   it('should find max bid amount', function () {
-    scope.bidder_id = '{}';
-    scope.auction_doc = {current_stage : 0};
-    scope.auction_doc.stages = [{amount:3}];
-    scope.auction_doc.minimalStep = {amount:1}
-    expect(scope.max_bid_amount()).toBe(2);
-    
+    rootScope.bidder_id = '{}';
+    rootScope.auction_doc = {current_stage : 0};
+    rootScope.auction_doc.stages = [{amount: 1240}];
+    rootScope.auction_doc.minimalStepPercentage = 0.006;
+    expect(scope.max_bid_amount()).toBe(1247.44);
   });
 //$scope.calculate_minimal_bid_amount
   it('should be Defined', function () {
     expect(scope.calculate_minimal_bid_amount).toBeDefined();
   });
   it('should find minimal bid', function () {
-    scope.auction_doc = {};
+    rootScope.auction_doc = {};
     scope.auction_doc.stages = [];
     scope.auction_doc.initial_bids = [{amount:1},{amount:2},{amount:3}];
     scope.calculate_minimal_bid_amount();
@@ -88,7 +95,7 @@ describe('auctionTest', function () {
     expect(scope.calculate_rounds).toBeDefined();
   });
   it('should work correct', function () {
-    scope.auction_doc = {stages : [{type:'pause'},{type:'pause'},5,{type:'pause'},{type:'pause'},{type:'pause'}]};
+    rootScope.auction_doc = {stages : [{type:'pause'},{type:'pause'},5,{type:'pause'},{type:'pause'},{type:'pause'}]};
     scope.calculate_rounds();
     expect(scope.Rounds).toEqual([0, 1, 3, 4, 5]);
   });
@@ -96,7 +103,7 @@ describe('auctionTest', function () {
   it('should be Defined', function () {
     expect(scope.scroll_to_stage).toBeDefined();
   });
-
+//
 //$scope.array
   it('should be Defined', function () {
     expect(scope.array).toBeDefined();
@@ -108,37 +115,19 @@ describe('auctionTest', function () {
   it('should be Defined', function () {
     expect(scope.open_menu).toBeDefined();
   });
-//$scope.calculate_bid_temp
-  it('should be Defined', function () {
-    expect(scope.calculate_bid_temp).toBeDefined();
-  });
-  it('should be Defined', function () {
-    scope.form.bid = 11;
-    scope.bidder_coeficient = 4;
-    scope.calculate_bid_temp();
-    expect(scope.form.bid_temp).toBe(11);
-    expect(scope.form.full_price).toBe(2.75);
-  });
 //$scope.calculate_full_price_temp
   it('should be Defined', function () {
     expect(scope.calculate_full_price_temp).toBeDefined();
   });
-  it('should work correct', function () {
-    scope.form.full_price = 11;
-    scope.bidder_coeficient = 3;
+  it('should work correct', angular.mock.inject(['AuctionUtils', function(AuctionUtils) {
+    rootScope.get_annual_costs_reduction = function(bidder_id) {};
+    rootScope.auction_doc = {noticePublicationDate: null, NBUdiscountRate:null};
+    AuctionUtils.npv = function() {return 11};
+    scope.form.BidsForm = {$valid: true};
+    rootScope.bidder_coeficient = 3;
     scope.calculate_full_price_temp();
-    expect(scope.form.full_price_temp).toBe(11);
-    expect(scope.form.bid).toBe(33);
-  });  
-//$scope.set_bid_from_temp
-  it('should be Defined', function () {
-    expect(scope.set_bid_from_temp).toBeDefined();
-  });
-  it('should work correct', function () {
-    scope.form.bid_temp = 0;
-    scope.set_bid_from_temp();
-    expect(scope.form.bid).toBe(0);
-  });
+    expect(scope.form.full_price_temp).toBe(33);
+  }]));
 
 
 /*//controller 'OffCanvasController'
